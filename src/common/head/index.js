bc64ccfc2b10f04b6117002a0816583343cf0fe8
Joe Zhang <1515037857@qq.com>
2019-08-23 17:01:21 +0800

    头部搜索框和按钮的编写

--- src/common/head/index.js
+++ src/common/head/index.js
@@ -1,12 +1,24 @@
 import React,{Component} from 'react'
-import {HeaderWrapper,Logo}  from "./style";
+import {HeaderWrapper,Logo,Nav,NavItem,NavSearch,Addition,Button}  from "./style";
 
 class Header extends Component{
     render() {
         return (
             <div>
-                <HeaderWrapper></HeaderWrapper>
-                <Logo></Logo>
+                <HeaderWrapper>
+                    <Logo></Logo>
+                    <Nav>
+                        <NavItem className='left active'>首页</NavItem>
+                        <NavItem className='left'>下载APP</NavItem>
+                        <NavSearch></NavSearch>
+                        <NavItem className='right'>登录</NavItem>
+                        <NavItem className='right'>Aa</NavItem>
+                    </Nav>
+                    <Addition>
+                        <Button>注册</Button>
+                        <Button>写文章</Button>
+                    </Addition>
+                </HeaderWrapper>
             </div>
         );
     }
